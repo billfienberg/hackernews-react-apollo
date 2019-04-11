831e6b6221d25e574d7f3e7e2a858e3c785f5bf2
Bill Fienberg <billfienberg@gmail.com>
2019-04-11 06:02:24 -0500

    Finsh step 1

--- server/src/generated/prisma-client/index.js
+++ server/src/generated/prisma-client/index.js
@@ -20,21 +20,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: ``,
-  secret: `mysecret123`
+  endpoint: `https://us1.prisma.sh/bill-fienberg-88b03b/hackernews-react-apollo-server/dev`
 });
 exports.prisma = new exports.Prisma();
-var models = [
-  {
-    name: "Link",
-    embedded: false
-  },
-  {
-    name: "User",
-    embedded: false
-  },
-  {
-    name: "Vote",
-    embedded: false
-  }
-];
